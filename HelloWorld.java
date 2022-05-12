648289d1cfd466a9f0bd09d8672cc8b5a0784543
AMWiltshire2003 <102810984+AMWiltshire2003@users.noreply.github.com>
2022-05-12 19:54:59 +0200

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        //printing the messange
+        System.out.println("HelloWorld.");
+         System.out.println(inputString);
+         System.out.println(click);
+    }
+}
